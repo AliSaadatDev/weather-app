5ffeff9739822b1aa574a1ab6333779b02271e13
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-08-19 23:44:56 +0330

    Add apps in conf

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'django_filters',
+    'accounts',
+    'weather',
+    'favorites',
 ]
 
 MIDDLEWARE = [
