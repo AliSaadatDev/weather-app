1c07d4de0d827b464f729ea897ee104d9cc5c1ad
AliSaadatDev <ali.saadat.dev@gmail.com>
2026-08-19 23:49:42 +0330

    Create weather view

--- backend/weather/views.py
+++ backend/weather/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .services import WeatherAPIClient
+
+
+class CurrentWeatherView(APIView):
+
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        city = request.query_params.get("city")
+
+        if not city:
+            return Response(
+                {
+                    "ok": False,
+                    "error": "city is required",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        client = WeatherAPIClient()
+
+        weather = client.get_current_weather(city)
+
+        return Response({
+            "ok": True,
+            "data": weather,
+        })
